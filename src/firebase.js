ea8d9459c676f0a37491a7667e0ac32ce4ec10a6
deepaksemwal121 <deepaksemwal121@gmail.com>
2021-06-23 14:24:18 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAPOfHEd2DQQ34-l-NGTORjOxz00lGYZz0",
+    authDomain: "clone-69218.firebaseapp.com",
+    projectId: "clone-69218",
+    storageBucket: "clone-69218.appspot.com",
+    messagingSenderId: "954549908251",
+    appId: "1:954549908251:web:c0d806b822cbf58f19d9be",
+    measurementId: "G-RQ48LR0YWW"
+  }; 
