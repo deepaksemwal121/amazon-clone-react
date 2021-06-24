daab216fb5414633e676ac42fb40b9fedf583072
deepaksemwal121 <deepaksemwal121@gmail.com>
2021-06-24 18:06:27 +0530

    Add Reducer and Dom

--- src/Product.js
+++ src/Product.js
@@ -1,6 +1,22 @@
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+
+  const addToBasket = () => {
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -18,7 +34,7 @@ function Product({ title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="book" />
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
